eba64f74ef23efca98c958c2633287630356eb7e
plm3710 <audgh3710@gmail.com>
2022-11-24 16:20:30 +0900

    5

--- run.py
+++ run.py
@@ -78,7 +78,7 @@ def construction():
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
-    run_simple('0.0.0.0', 9001, app)
+    run_simple('0.0.0.0', 9002, app)
 
 
 # In[ ]:
